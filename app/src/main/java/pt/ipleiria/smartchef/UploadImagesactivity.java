f0cb65415781dd08e758ba57e97d1b3684c1be1c
gabrielchanchay <gdchanchay@live.com>
2019-05-29 21:49:19 +0100

    select adn charge theimages on the view

--- app/src/main/java/pt/ipleiria/smartchef/UploadImagesactivity.java
+++ app/src/main/java/pt/ipleiria/smartchef/UploadImagesactivity.java
@@ -3,16 +3,24 @@ package pt.ipleiria.smartchef;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 public class UploadImagesactivity extends AppCompatActivity {
 
@@ -22,6 +30,12 @@ public class UploadImagesactivity extends AppCompatActivity {
     public static final int CAMERA_IMAGE_REQUEST = 3;
     public static final String FILE_NAME = "temp.jpg";
 
+    private static final String TAG = UploadImagesactivity.class.getSimpleName();
+
+    private int imageNumber=0;
+
+    private static Logger log= Logger.getLogger("log");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +43,10 @@ public class UploadImagesactivity extends AppCompatActivity {
     }
 
     public void selectSource(View view){
+        log.warning(String.valueOf(view.getId()));
+//        if(){
+
+//        }
         AlertDialog.Builder builder = new AlertDialog.Builder(UploadImagesactivity.this);
         builder.setMessage(R.string.dialog_select_prompt)
                 .setPositiveButton(R.string.dialog_select_gallery, new DialogInterface.OnClickListener() {
@@ -75,4 +93,67 @@ public class UploadImagesactivity extends AppCompatActivity {
         return new File(dir, FILE_NAME);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            ImageView imageView = findViewById(R.id.image1);
+            try{
+
+
+            Bitmap bitmap = scaleBitmapDown(
+                    MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData()),
+                    1200);
+            imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                Log.d(TAG, "Image picking failed because " + e.getMessage());
+                Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
+            }
+//            uploadImage(data.getData());
+        } else if (requestCode == CAMERA_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            Uri photoUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", getCameraFile());
+//            uploadImage(photoUri);
+        }
+
+
+    }
+
+    public Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_PERMISSIONS_REQUEST:
+                if (PermissionUtils.permissionGranted(requestCode, CAMERA_PERMISSIONS_REQUEST, grantResults)) {
+                    startCamera();
+                }
+                break;
+            case GALLERY_PERMISSIONS_REQUEST:
+                if (PermissionUtils.permissionGranted(requestCode, GALLERY_PERMISSIONS_REQUEST, grantResults)) {
+                    startGalleryChooser();
+                }
+                break;
+        }
+    }
 }
