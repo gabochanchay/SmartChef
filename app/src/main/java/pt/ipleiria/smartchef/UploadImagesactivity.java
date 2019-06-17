480b31fc700137cb186b4d72e52142604f880d5f
gabrielchanchay <2180247@my.ipleiria.pt>
2019-06-17 23:45:37 +0100

    removing unused code

--- app/src/main/java/pt/ipleiria/smartchef/UploadImagesactivity.java
+++ app/src/main/java/pt/ipleiria/smartchef/UploadImagesactivity.java
@@ -588,7 +588,7 @@ public class UploadImagesactivity extends AppCompatActivity {
         String url = "https://api.uclassify.com/v1/uclassify/iab-taxonomy/classify?readkey=BaCk5w4RQ4y2&text="+word;
 //    log.warning(url);
 
-        
+
         final boolean food=false;
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.GET,
